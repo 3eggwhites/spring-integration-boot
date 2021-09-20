b976d55f9220f771f084dee75de40ab8ad1cfbed
Ayan Pal <ayan.pal@live.com>
2021-09-20 21:54:04 +0530

    first commit

---
+++ src/main/java/com/integration/boot/sprintintegrationboot/SprintIntegrationBootApplication.java
@@ -0,0 +1,13 @@
+package com.integration.boot.sprintintegrationboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintIntegrationBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintIntegrationBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/integration/boot/sprintintegrationboot/SprintIntegrationBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.boot.sprintintegrationboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintIntegrationBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
